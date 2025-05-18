8e44d3ae3545481a1968c68e681eb8d54e87a8bc
Pedro Paulo Durand <pedropaulodurand@gmail.com>
2025-05-18 16:32:08 -0300

    commit inicial

---
+++ backend/src/main/java/edu/example/ConsultaAgil/ConsultaAgilApplication.java
@@ -0,0 +1,13 @@
+package edu.example.ConsultaAgil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaAgilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaAgilApplication.class, args);
+	}
+
+}
