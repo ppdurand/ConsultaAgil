1b6b16b44ba499fdf8bf0cb42970d0a5ad948482
Pedro Paulo Durand <pedropaulodurand@gmail.com>
2025-05-20 23:12:13 -0300

    tabela de pacientes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <RegisterPatient />
-      {/* <ListPatients /> */}
+      <ListPatients />
     </>
   )
 }
--- frontend/src/pages/ListPatients/index.jsx
+++ frontend/src/pages/ListPatients/index.jsx
@@ -1,47 +1,52 @@
+import { useEffect, useState } from "react";
 import { Table } from "antd";
 
-export function ListPatients(){
+export function ListPatients() {
+    const [patients, setPatients] = useState([]);
 
-    const patients = fetch('http://localhost:8080/patient/list')
-        .then((res) => {
-            if (!res.ok) {
-                const error = res.json()
-                throw new Error(error.message || 'Erro ao buscar pacientes');
-            }
-            return res.json()
-        })
-        .then((data) => {
-            console.log(data);
-            return data
-        })
-        .catch((err) => {
-            alert(err.message, "complicado")
-        })
-    
-    const columns = [{
-        title: 'Nome',
-        dataIndex: 'name',
-        key: 'name',
-    },
-    {
-        title: 'Telefone',
-        dataIndex: 'telephone',
-        key: 'telephone',
-    },
-    {
-        title: 'Ações',
-        key: 'action',
-        render: (_, record) => (
-            <span>
-                <a>Editar</a>
-                <a style={{ marginLeft: 16 }}>Excluir</a>
-            </span>
-        ),
-    }]
+    useEffect(() => {
+        fetch('http://localhost:8080/patient/list')
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('Erro ao buscar pacientes');
+                }
+                return res.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setPatients(data);
+            })
+            .catch((err) => {
+                alert("Erro: " + err.message);
+            });
+    }, []);
+
+    const columns = [
+        {
+            title: 'Nome',
+            dataIndex: 'nome',
+            key: 'nome',
+        },
+        {
+            title: 'Telefone',
+            dataIndex: 'telephone',
+            key: 'telephone',
+        },
+        {
+            title: 'Ações',
+            key: 'action',
+            render: (_, record) => (
+                <span>
+                    <a>Editar</a>
+                    <a style={{ marginLeft: 16 }}>Excluir</a>
+                </span>
+            ),
+        }
+    ];
 
     return (
         <div className="table-container">
-            <Table dataSource={patients} columns={columns} />
+            <Table dataSource={patients} columns={columns} rowKey="id" />
         </div>
-    )
+    );
 }
